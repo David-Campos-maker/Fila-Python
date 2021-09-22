c9c425691418502baf669447f78d84d2b3aac440
David-Campos-maker <70824817+David-Campos-maker@users.noreply.github.com>
2021-09-22 13:14:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from Fila import Fila
+
+minhaFila = Fila()
+
+minhaFila.add_on_row(minhaFila , 1)
+minhaFila.add_on_row(minhaFila , 2)
+minhaFila.add_on_row(minhaFila , 3)
+
+minhaFila.print_row(minhaFila)
+
+minhaFila.delet(minhaFila)
+
+print()
+
+minhaFila.print_row(minhaFila)
